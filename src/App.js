ba59d8bce91cb7ef07b901e89edca6702e60c570
Dannsln <dannsln.lune@gmail>
2026-04-17 16:43:17 -0500

    .

--- src/App.js
+++ src/App.js
@@ -327,10 +327,10 @@ const DEFAULT_STAFF = [
 ];
 
 const ROLE_INFO = {
- admin:    { label:"Administrador", color:"#FFD700",},
- cajero:   { label:"Cajero",        color:"#3498db",},
- mesero:   { label:"Mesero",        color:"#27ae60",},
- cocinero: { label:"Cocina",        color:"#e67e22",},
+ admin:    { label:"Administrador", },
+ cajero:   { label:"Cajero",   },
+ mesero:   { label:"Mesero",    },
+ cocinero: { label:"Cocina",      },
 };
 
 // ═══════════════════════════════════════════════════════════════════
