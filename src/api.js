7df6e181b6f72a1a12a5849df99d64aa40a25a4c
Dannsln <dannsln.lune@gmail>
2026-04-29 14:09:31 -0500

    asdas

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = (process.env.REACT_APP_BACKEND_URL || "http://localhost:5000").replace(/\/$/, "");
+export const API_BASE_URL = (process.env.REACT_APP_BACKEND_URL || "https://backend-production-6d7e.up.railway.app").replace(/\/$/, "");
 
 export const getAuthToken = () => localStorage.getItem("token");
 
